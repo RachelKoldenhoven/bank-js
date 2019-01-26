5fadc2be521ef9feae0b50f0890f87c7622a740f
RachelKoldenhoven <rachel@koldenfrozen.com>
2019-01-26 16:47:37 -0700

    add comments, remove sample test

--- src/bank.js
+++ src/bank.js
@@ -16,18 +16,20 @@ class Bank {
         }
     }
 
-    sample() {
-        return true;
-    }
-
     parse() {
+        // get big string from file
+        // could not get space at end of line, added on at begin of line 2 in txt file
         let entries = fs.readFileSync('entries.txt', 'utf8');
+        // remove newline chars and change to letters
         let a = entries.replace(/\n/g, '').replace(/ /g, 'S').replace(/_/g, 'U').replace(/\|/g, 'P');
+        // break string into array of threes
         let b = a.match(/.{1,3}/g);
+        // put all triplets for each num together
         let codes = ['', '', '', '', '', '', '', '', ''];
         for (let i = 0; i < codes.length; i++) {
             codes[i] = codes[i].concat(b[i]).concat(b[i + 9]).concat(b[i + 18]);
         }
+        // sub code for its num
         return codes.map(code => this.key[code]);
     }
 }
--- test/bankSpec.js
+++ test/bankSpec.js
@@ -2,27 +2,9 @@ const Bank = require('../src/bank.js');
 const expect = require('chai').expect;
 
 describe('Bank', () => {
-    it('should return true', () => {
-        // setup
-        let bank = new Bank();
-        const expected = true;
-
-        // exercise
-        let actual = bank.sample();
-
-        // assert
-        expect(actual).to.equal(expected);
-    });
-
     it('should return an account number', () => {
         // setup
         const bank = new Bank();
-        const input = [
-            ['    _  _     _  _  _  _  _ '],
-            ['  | _| _||_||_ |_   ||_||_|'],
-            ['  ||_  _|  | _||_|  ||_| _|'],
-            []
-        ];
         const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
         // exercise
