eb1bb5f8d60076cdda238085af4d9cbf5e4a2be6
RachelKoldenhoven <rachel@koldenfrozen.com>
2019-01-26 16:42:39 -0700

    passes most basic case

--- src/bank.js
+++ src/bank.js
@@ -1,6 +1,34 @@
+const fs = require('fs');
+
 class Bank {
+    constructor() {
+        this.key = {
+            'SSSSSPSSP': 1,
+            'SUSSUPPUS': 2,
+            'SUSSUPSUP': 3,
+            'SSSPUPSSP': 4,
+            'SUSPUSSUP': 5,
+            'SUSPUSPUP': 6,
+            'SUSSSPSSP': 7,
+            'SUSPUPPUP': 8,
+            'SUSPUPSUP': 9,
+            'SUSPSPPUP': 0
+        }
+    }
+
     sample() {
         return true;
+    }
+
+    parse() {
+        let entries = fs.readFileSync('entries.txt', 'utf8');
+        let a = entries.replace(/\n/g, '').replace(/ /g, 'S').replace(/_/g, 'U').replace(/\|/g, 'P');
+        let b = a.match(/.{1,3}/g);
+        let codes = ['', '', '', '', '', '', '', '', ''];
+        for (let i = 0; i < codes.length; i++) {
+            codes[i] = codes[i].concat(b[i]).concat(b[i + 9]).concat(b[i + 18]);
+        }
+        return codes.map(code => this.key[code]);
     }
 }
 
--- test/bankSpec.js
+++ test/bankSpec.js
@@ -12,5 +12,23 @@ describe('Bank', () => {
 
         // assert
         expect(actual).to.equal(expected);
+    });
+
+    it('should return an account number', () => {
+        // setup
+        const bank = new Bank();
+        const input = [
+            ['    _  _     _  _  _  _  _ '],
+            ['  | _| _||_||_ |_   ||_||_|'],
+            ['  ||_  _|  | _||_|  ||_| _|'],
+            []
+        ];
+        const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        // exercise
+        const actual = bank.parse();
+
+        // assert
+        expect(actual).to.deep.equal(expected);
     })
 });
